e8b0f8c39d48cf950fcc15a5ad1e57f6695a3dfc
Niveydh Pai <niveydhpai@Niveydhs-MacBook-Air.local>
2025-07-19 21:18:56 -0700

    Added prototype roulette mode

--- spinning_wheel.py
+++ spinning_wheel.py
@@ -1,4 +1,3 @@
-# --------------- LIBRARIES ----------------
 import board
 import time
 import digitalio
@@ -6,49 +5,91 @@ import pwmio
 
 # ---------------- SETUP -------------------
 
-# Motor Pins to control direction (IN1 and IN2)
+# Motor direction control
 in1 = digitalio.DigitalInOut(board.D2)
 in1.direction = digitalio.Direction.OUTPUT
 
 in2 = digitalio.DigitalInOut(board.D3)
 in2.direction = digitalio.Direction.OUTPUT
 
-# PWM on ENA pin to control the speed
+# PWM for motor speed
 pwm = pwmio.PWMOut(board.D6, frequency=1000)
 
-# Touch Sensor for digital input
-touch = digitalio.DigitalInOut(board.D7)
-touch.direction = digitalio.Direction.INPUT
+# Buttons
+touch_button = digitalio.DigitalInOut(board.D7)
+touch_button.direction = digitalio.Direction.INPUT
 
-'''
-    Set direction:
-    IN1 and IN2 == False           --> Motor OFF
-    IN1 == True and IN2 == False   --> Forward
-    IN1 == False and IN2 == True   --> Backwards
-    IN1 and IN2 == True            --> Motor OFF
-'''
+roulette_button = digitalio.DigitalInOut(board.D4)
+roulette_button.direction = digitalio.Direction.INPUT
+
+# Hall sensor
+hall_sensor = digitalio.DigitalInOut(board.D8)
+hall_sensor.direction = digitalio.Direction.INPUT
+hall_sensor.pull = digitalio.Pull.UP
+
+# LEDs
+led_red = digitalio.DigitalInOut(board.D9)
+led_red.direction = digitalio.Direction.OUTPUT
+
+led_green = digitalio.DigitalInOut(board.D10)
+led_green.direction = digitalio.Direction.OUTPUT
+
+# Motor direction setup
 in1.value = True
 in2.value = False
 
-# --------------- MAIN LOOP ----------------
-while True:
-    if touch.value:  # Sensor outputs HIGH when touched
+# ---------------- FUNCTIONS -------------------
 
-        # Start at the full speed
-        speed = 65535
+def set_led_color(color):
+    if color == "green":
+        led_red.value = False
+        led_green.value = True
+    elif color == "red":
+        led_red.value = True
+        led_green.value = False
+    else:
+        led_red.value = False
+        led_green.value = False
+
+def spin_motor_and_decelerate():
+    speed = 65535
+    pwm.duty_cycle = speed
+    print("Wheel spinning...")
+
+    while speed > 7500:
         pwm.duty_cycle = speed
+        time.sleep(0.2)
+        speed = int(speed * 0.95)
 
-        # Speed can be changed, but my motor doesn't move after ~7500
-        while speed > 7500:
-            pwm.duty_cycle = speed
-            time.sleep(0.2) # For smooth slow down
-            speed = int(speed * 0.95) # Motor loses 5% of it's speed every loop
+    pwm.duty_cycle = 0
+    print("Wheel stopped.")
 
-        # Full Stop
-        pwm.duty_cycle = 0
+# ---------------- MAIN LOOP -------------------
 
-        # Wait until sensor is released
-        while touch.value:
+while True:
+    if touch_button.value:  # Normal mode
+        spin_motor_and_decelerate()
+
+        # Wait for release
+        while touch_button.value:
             time.sleep(0.1)
 
-        time.sleep(1)  # Cooldown before next touch
+        time.sleep(1) 
+
+    elif roulette_button.value:  # Roulette mode
+        spin_motor_and_decelerate()
+
+        # Check Hall sensor
+        aligned = hall_sensor.value == 0
+        if aligned:
+            set_led_color("green")
+        else:
+            set_led_color("red")
+
+        time.sleep(3)  # Show result
+
+        # Wait for release
+        while roulette_button.value:
+            time.sleep(0.1)
+
+        time.sleep(1)  # Cooldown
