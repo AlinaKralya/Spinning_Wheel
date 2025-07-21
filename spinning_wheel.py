a10fbdcd70318964a5c46ae5d20d8c0ad66526a4
AlinaKralya <alinakralya04@gmail.com>
2025-07-21 16:14:32 -0700

    Updated modes to switch depending on how long you touch the sensor

--- spinning_wheel.py
+++ spinning_wheel.py
@@ -1,3 +1,4 @@
+# --------------- LIBRARIES ----------------
 import board
 import time
 import digitalio
@@ -5,24 +6,21 @@ import pwmio
 
 # ---------------- SETUP -------------------
 
-# Motor direction control
+# Motor Pins to control direction (IN1 and IN2)
 in1 = digitalio.DigitalInOut(board.D2)
 in1.direction = digitalio.Direction.OUTPUT
 
 in2 = digitalio.DigitalInOut(board.D3)
 in2.direction = digitalio.Direction.OUTPUT
 
-# PWM for motor speed
+# PWM on ENA pin to control the speed
 pwm = pwmio.PWMOut(board.D6, frequency=1000)
 
-# Buttons
+# Touch Sensor for digital input
 touch_button = digitalio.DigitalInOut(board.D7)
 touch_button.direction = digitalio.Direction.INPUT
 
-roulette_button = digitalio.DigitalInOut(board.D4)
-roulette_button.direction = digitalio.Direction.INPUT
-
-# Hall sensor
+# Hall Magnetic sensor
 hall_sensor = digitalio.DigitalInOut(board.D8)
 hall_sensor.direction = digitalio.Direction.INPUT
 hall_sensor.pull = digitalio.Pull.UP
@@ -34,63 +32,77 @@ led_red.direction = digitalio.Direction.OUTPUT
 led_green = digitalio.DigitalInOut(board.D10)
 led_green.direction = digitalio.Direction.OUTPUT
 
-# Motor direction setup
+led_blue = digitalio.DigitalInOut(board.D11)
+led_blue.direction = digitalio.Direction.OUTPUT
+
+'''
+    Set direction:
+    IN1 and IN2 == False           --> Motor OFF
+    IN1 == True and IN2 == False   --> Forward
+    IN1 == False and IN2 == True   --> Backwards
+    IN1 and IN2 == True            --> Motor OFF
+'''
 in1.value = True
 in2.value = False
 
-# ---------------- FUNCTIONS -------------------
+time_passed = time.monotonic()
 
-# Sets different LED colors based on Magnet Position
-def set_led_color(color):
-    if color == "green":
-        led_red.value = False
-        led_green.value = True
-    elif color == "red":
-        led_red.value = True
-        led_green.value = False
-    else:
-        led_red.value = False
-        led_green.value = False
+# --------------- FUNCTIONS ----------------
 
-# Controls the Motor to spin and decelerate
 def spin_motor_and_decelerate():
     speed = 65535
     pwm.duty_cycle = speed
-    #7500 the threshold speed
+
     while speed > 7500:
         pwm.duty_cycle = speed
         time.sleep(0.2)
-        # goes down 5% each time
         speed = int(speed * 0.95)
 
     pwm.duty_cycle = 0
 
-# ---------------- MAIN LOOP -------------------
+def set_led_color(color):
+    if color == "green":
+        led_green.value = True
+        led_red.value = False
+    elif color == "red":
+        led_green.value = False
+        led_red.value = True
+    else:
+        led_red.value = False
+        led_green.value = False 
 
+
+# --------------- MAIN LOOP ----------------
 while True:
-    if touch_button.value:  # Normal mode
-        spin_motor_and_decelerate()
+    if touch_button.value:
+        press_start = time.monotonic()
 
-        # Wait for release
+        # Blink LED while holding the button
         while touch_button.value:
-            time.sleep(0.1)
+            led_blue.value = True
+            time.sleep(0.25)
+            led_blue.value = False
+            time.sleep(0.25)
 
-        time.sleep(1) 
+        press_duration = time.monotonic() - press_start
 
-    elif roulette_button.value:  # Roulette mode
-        spin_motor_and_decelerate()
+        if press_duration >= 1.0:
+            # Roulette mode
+            spin_motor_and_decelerate()
 
-        # Check Hall sensor
-        aligned = hall_sensor.value == 0
-        if aligned:
-            set_led_color("green")
+            time.sleep(1.0)  # give wheel time to settle
+
+            aligned = hall_sensor.value == 0
+            if aligned:
+                set_led_color("green")
+            else:
+                set_led_color("red")
+
+            time.sleep(3)  # Show result
         else:
-            set_led_color("red")
+            # Trivia
+            spin_motor_and_decelerate()
 
-        time.sleep(3)  # Show result
-
-        # Wait for release
-        while roulette_button.value:
-            time.sleep(0.1)
-
-        time.sleep(1)  # Cooldown
+        # Turn off LEDs and cooldown
+        set_led_color("off")
+        time.sleep(1)
